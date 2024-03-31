ada6819fe9bb01ca6dcf1c659655968922830d7e
Somya Kumari <charanpahadisoumya@gmail.com>
2024-03-31 20:21:55 +0530

    adding extra features in the 07Reactrouter project

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,28 +2,53 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './Component/Home/home.jsx'
 import About from './Component/About/about.jsx'
+import Contact from './Component/Contact/contact.jsx'
+import User from './Component/User/user.jsx'
+import Github, { githubInfoLoader } from './Component/Github/Github.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout/>,
-    children:[{
-      path: "",
-      element: <Home/>
-    },
-    {
-      path: "about",
-      element: <About/>
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children:[{
+//       path: "",
+//       element: <Home/>
+//     },
+//     {
+//       path: "about",
+//       element: <About/>
 
-    }
+//     },
   
-  ]
-  }
-])
+//     {
+//       path: "contact",
+//       element: <Contact/>
+
+//     }
+  
+//   ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+    <Route path='' element={<Home />} />
+    <Route path='about' element={<About />} />
+    <Route path='contact' element={<Contact />} />
+    <Route path='user/:userid' element={<User />} />
+    <Route 
+    loader={githubInfoLoader}
+    path='github' 
+    element={<Github />}
+     />
+  </Route>
+)
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
